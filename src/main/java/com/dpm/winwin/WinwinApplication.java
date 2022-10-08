51cff463e52d110e656ae0c8e741315c4678d77c
choiyoungkwon12 <hpwh4457@naver.com>
2022-10-08 14:36:39 +0900

    intial commit

---
+++ src/main/java/com/dpm/winwin/WinwinApplication.java
@@ -0,0 +1,13 @@
+package com.dpm.winwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinwinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinwinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dpm/winwin/WinwinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpm.winwin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinwinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

19f4cc6aa7e6d8a4497b48b331fec71c1c0b82b8
minsoozz <minsoo1604@naver.com>
2022-11-01 01:27:26 +0900

    chore: ComponentScan 설정 및 라이브러리 Bean 등록
    MainApplication 실행 시 스캔 해야 할 Bean 설정 및 JPA,QueryDSL 관련 Bean 등록
    
    issue: #9

--- winwin-be-api/src/main/java/com/dpm/winwin/api/WinwinApiApplication.java
+++ winwin-be-api/src/main/java/com/dpm/winwin/api/WinwinApiApplication.java
@@ -2,11 +2,20 @@ package com.dpm.winwin.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.dpm.winwin.domain.repository")
+@EntityScan("com.dpm.winwin.domain.entity")
+@ComponentScan(basePackages = {
+    "com.dpm.winwin.api",
+    "com.dpm.winwin.domain"
+})
 public class WinwinApiApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(WinwinApiApplication.class);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(WinwinApiApplication.class);
+    }
 }

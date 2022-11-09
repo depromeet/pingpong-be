adba59346c19b9110d09a808662f4e4863d662bc
hpwh4457@naver.com <ned.choi@kakaocorp.com>
2022-11-09 19:58:50 +0900

    chore : jasypt 설정 위치 변경
    api 대신 domain 모듈에서 설정할 수 있도록 변경하였습니다.

--- winwin-be-chatting/src/main/java/com/dpm/winwin/chatting/WinwinChattingApplication.java
+++ winwin-be-chatting/src/main/java/com/dpm/winwin/chatting/WinwinChattingApplication.java
@@ -2,10 +2,18 @@ package com.dpm.winwin.chatting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+<<<<<<< HEAD
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+=======
+import org.springframework.context.annotation.ComponentScan;
+>>>>>>> c2cb0cb (chore : jasypt 설정 위치 변경)
 
+@ComponentScan(basePackages = {
+        "com.dpm.winwin.chatting",
+        "com.dpm.winwin.domain"
+})
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.dpm.winwin.domain.repository")
 @EntityScan("com.dpm.winwin.domain.entity")
--- winwin-be-domain/src/main/java/com/dpm/winwin/domain/config/JasyptConfig.java
+++ winwin-be-domain/src/main/java/com/dpm/winwin/domain/config/JasyptConfig.java
@@ -1,4 +1,4 @@
-package com.dpm.winwin.api.config;
+package com.dpm.winwin.domain.config;
 
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;

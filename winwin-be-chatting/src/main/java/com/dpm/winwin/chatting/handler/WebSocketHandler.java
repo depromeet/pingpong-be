e7e715ce04ee8b29de6a1bf105d3376de2d4f5dc
yeonhee <ydol2@naver.com>
2022-11-10 00:24:52 +0900

    feat : 웹소켓 configuration 및 handler 구현
    웹소켓 configuration와 handler 틀을 구현하였습니다. handler는 추가 구현이 필요합니다.

---
+++ winwin-be-chatting/src/main/java/com/dpm/winwin/chatting/handler/WebSocketHandler.java
@@ -0,0 +1,11 @@
+package com.dpm.winwin.chatting.handler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@RequiredArgsConstructor
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+}

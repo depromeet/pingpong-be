fdb466ea0b35a88608b437ecc5d437c2492b798f
minsoozz <minsoo1604@naver.com>
2022-11-13 00:37:38 +0900

    feat: 각 모듈에서 사용할 공통 반환 객체 설정

---
+++ winwin-be-api/src/main/java/com/dpm/winwin/api/common/error/exception/custom/BusinessException.java
@@ -0,0 +1,14 @@
+package com.dpm.winwin.api.common.error.exception.custom;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+  private final int code;
+
+  public BusinessException(int code, String phrase) {
+    super(phrase);
+    this.code = code;
+  }
+}

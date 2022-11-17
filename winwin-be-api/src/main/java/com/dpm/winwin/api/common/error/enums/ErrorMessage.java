a518d441f68dde2ad01e8e6aa214300c005f93a0
yeonhee <ydol2@naver.com>
2022-11-17 22:49:27 +0900

    feat : 회원 not found 메시지 추가

--- winwin-be-api/src/main/java/com/dpm/winwin/api/common/error/enums/ErrorMessage.java
+++ winwin-be-api/src/main/java/com/dpm/winwin/api/common/error/enums/ErrorMessage.java
@@ -1,6 +1,7 @@
 package com.dpm.winwin.api.common.error.enums;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -8,7 +9,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorMessage {
-  INTERVAL_SERVER_ERROR(INTERNAL_SERVER_ERROR.value(), "요청을 처리하는 과정에서 서버가 예상하지 못한 오류가 발생하였습니다.");
+  INTERVAL_SERVER_ERROR(INTERNAL_SERVER_ERROR.value(), "요청을 처리하는 과정에서 서버가 예상하지 못한 오류가 발생하였습니다."),
+
+  MEMBER_NOT_FOUND(NOT_FOUND.value(), "해당 회원을 찾을 수 없습니다.");
 
   private final int code;
   private final String phrase;

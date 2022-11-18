f84f255efb8063db63d9595c53ebfd01a0a03624
minsoozz <minsoo1604@naver.com>
2022-11-18 20:58:55 +0900

    feat: 포스트가 존재하지 않는 경우 ErrorMessage 추가

--- winwin-be-api/src/main/java/com/dpm/winwin/api/common/error/enums/ErrorMessage.java
+++ winwin-be-api/src/main/java/com/dpm/winwin/api/common/error/enums/ErrorMessage.java
@@ -13,7 +13,8 @@ public enum ErrorMessage {
   MEMBER_NOT_FOUND(NOT_FOUND.value(), "해당 회원을 찾을 수 없습니다."),
   MAIN_CATEGORY_NOT_FOUND(NOT_FOUND.value(), "해당 대분류 카테고리를 찾을 수 없습니다."),
   MID_CATEGORY_NOT_FOUND(NOT_FOUND.value(), "해당 중분류 카테고리를 찾을 수 없습니다."),
-  SUB_CATEGORY_NOT_FOUND(NOT_FOUND.value(), "해당 소분류 카테고리를 찾을 수 없습니다.");
+  SUB_CATEGORY_NOT_FOUND(NOT_FOUND.value(), "해당 소분류 카테고리를 찾을 수 없습니다."),
+  POST_NOT_FOUND(NOT_FOUND.value(), "해당 포스트를 찾을 수 없습니다.");
 
   private final int code;
   private final String phrase;

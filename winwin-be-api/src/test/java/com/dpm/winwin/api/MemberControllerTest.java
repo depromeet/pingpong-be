77d9e45ae96829d3f17049466bf54844c1d4e3fe
yeonhee <ydol2@naver.com>
2022-11-19 18:15:20 +0900

    test : member 테스트 임시 @Disabled 처리

--- winwin-be-api/src/test/java/com/dpm/winwin/api/MemberControllerTest.java
+++ winwin-be-api/src/test/java/com/dpm/winwin/api/MemberControllerTest.java
@@ -1,5 +1,6 @@
 package com.dpm.winwin.api;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,32 +26,33 @@ public class MemberControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-//    @Test
-//    @DisplayName("아직 데이터가 없어서 나중에 사용할 회원 조회 테스트 ")
-//    public void memberGetTest() throws Exception {
-//        // 조회 API -> 대상의 데이터가 있어야 작동
-//        mockMvc.perform(
-//                        get("/api/v1/member/{memberId}", 1L)
-//                                .contentType(MediaType.APPLICATION_JSON)
-//                )
-//                .andExpect(status().isOk())
-//                .andDo(
-//                        document("member-controller-test",
-//                                pathParameters(
-//                                        parameterWithName("memberId").description("Member ID")
-//                                ),
-//                                responseFields(
-//                                        fieldWithPath("memberId").description("회원의 id"),
-//                                        fieldWithPath("nickname").description("회원의 닉네임"),
-//                                        fieldWithPath("image").description("회원의 이미지"),
-//                                        fieldWithPath("introductions").description("소개글"),
-//                                        fieldWithPath("exchangeCount").description("재능 교환 횟수"),
-//                                        fieldWithPath("profileLink").description("프로필 링크"),
-//                                        fieldWithPath("givenTalent").description("주고싶은 재능"),
-//                                        fieldWithPath("takenTalent").description("받고싶은 재능")
-//                                )
-//                        )
-//                )
-//        ;
-//    }
+    @Test
+    @Disabled
+    @DisplayName("아직 데이터가 없어서 나중에 사용할 회원 조회 테스트 ")
+    public void memberGetTest() throws Exception {
+        // 조회 API -> 대상의 데이터가 있어야 작동
+        mockMvc.perform(
+                        get("/api/v1/member/{memberId}", 1L)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andDo(
+                        document("member-controller-test",
+                                pathParameters(
+                                        parameterWithName("memberId").description("Member ID")
+                                ),
+                                responseFields(
+                                        fieldWithPath("memberId").description("회원의 id"),
+                                        fieldWithPath("nickname").description("회원의 닉네임"),
+                                        fieldWithPath("image").description("회원의 이미지"),
+                                        fieldWithPath("introductions").description("소개글"),
+                                        fieldWithPath("exchangeCount").description("재능 교환 횟수"),
+                                        fieldWithPath("profileLink").description("프로필 링크"),
+                                        fieldWithPath("givenTalent").description("주고싶은 재능"),
+                                        fieldWithPath("takenTalent").description("받고싶은 재능")
+                                )
+                        )
+                )
+        ;
+    }
 }

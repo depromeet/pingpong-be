2b1ce044ee6d2dd29ad2c86453b50c118d0a250c
sa46lll <9812smh@naver.com>
2022-11-24 21:38:13 +0900

    feat: 검색되는 포스트가 없는 경우 ErrorMessage 추가
    검색 필터에 해당하는 포스트가 없는 경우를 위해 204 코드의 에러 메세지를 추가하였습니다.

--- winwin-be-api/src/main/java/com/dpm/winwin/api/common/error/enums/ErrorMessage.java
+++ winwin-be-api/src/main/java/com/dpm/winwin/api/common/error/enums/ErrorMessage.java
@@ -2,6 +2,7 @@ package com.dpm.winwin.api.common.error.enums;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +16,8 @@ public enum ErrorMessage {
   MID_CATEGORY_NOT_FOUND(NOT_FOUND.value(), "해당 중분류 카테고리를 찾을 수 없습니다."),
   SUB_CATEGORY_NOT_FOUND(NOT_FOUND.value(), "해당 소분류 카테고리를 찾을 수 없습니다."),
   POST_NOT_FOUND(NOT_FOUND.value(), "해당 포스트를 찾을 수 없습니다."),
-  TALENT_NOT_FOUND(NOT_FOUND.value(), "해당 받고 싶은 재능을 찾을 수 없습니다.");
+  TALENT_NOT_FOUND(NOT_FOUND.value(), "해당 받고 싶은 재능을 찾을 수 없습니다."),
+  POST_NO_CONTENT(NO_CONTENT.value(), "존재하는 게시물이 없습니다.");
 
   private final int code;
   private final String phrase;

40f922e99899cfb4214ef0234854649d211ee1a2
minsoozz <minsoo1604@naver.com>
2022-11-28 00:26:42 +0900

    refactor: CategoryId -> Category 네이밍 변경

--- winwin-be-api/src/main/java/com/dpm/winwin/api/post/dto/request/PostUpdateRequest.java
+++ winwin-be-api/src/main/java/com/dpm/winwin/api/post/dto/request/PostUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.dpm.winwin.api.post.dto.request;
 
+import com.dpm.winwin.domain.dto.post.PostUpdateDto;
 import com.dpm.winwin.domain.entity.post.enums.ExchangePeriod;
 import com.dpm.winwin.domain.entity.post.enums.ExchangeTime;
 import com.dpm.winwin.domain.entity.post.enums.ExchangeType;
@@ -10,9 +11,9 @@ public record PostUpdateRequest(
     String title,
     String content,
     boolean isShare,
-    long mainCategory,
-    long midCategory,
-    long subCategory,
+    Long mainCategory,
+    Long midCategory,
+    Long subCategory,
     List<LinkRequest> links,
     List<Long> takeCategories,
     String takenContent,
@@ -22,7 +23,13 @@ public record PostUpdateRequest(
     ExchangeTime exchangeTime
 ) {
 
-    public List<LinkRequest> getExistentLinks(){
+    public PostUpdateDto toDto() {
+        return new PostUpdateDto(this.title, this.content, this.isShare,
+            this.links().stream().map(LinkRequest::toDto).toList(), this.takenContent,
+            this.chatLink, this.exchangeType, this.exchangePeriod, this.exchangeTime);
+    }
+
+    public List<LinkRequest> getExistentLinks() {
         return this.links()
             .stream()
             .filter(link -> link.id() != null)

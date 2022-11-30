6b4037bd1e8a02a817210a540d7f1b2aa6e87998
yeonhee <ydol2@naver.com>
2022-11-30 13:29:01 +0900

    feat : 회원 수정 로직 구현

--- winwin-be-api/src/main/java/com/dpm/winwin/api/member/application/MemberCommandService.java
+++ winwin-be-api/src/main/java/com/dpm/winwin/api/member/application/MemberCommandService.java
@@ -1,12 +1,22 @@
 package com.dpm.winwin.api.member.application;
 
+import com.dpm.winwin.api.common.error.enums.ErrorMessage;
 import com.dpm.winwin.api.common.error.exception.custom.BusinessException;
 import com.dpm.winwin.api.member.dto.request.MemberCreateRequest;
 import com.dpm.winwin.api.member.dto.request.MemberUpdateRequest;
 import com.dpm.winwin.api.member.dto.response.MemberCreateResponse;
 import com.dpm.winwin.api.member.dto.response.MemberUpdateResponse;
+import com.dpm.winwin.api.post.dto.request.LinkRequest;
+import com.dpm.winwin.api.post.dto.request.PostUpdateRequest;
+import com.dpm.winwin.api.post.dto.response.LinkResponse;
+import com.dpm.winwin.api.post.dto.response.PostUpdateResponse;
+import com.dpm.winwin.domain.entity.category.MainCategory;
+import com.dpm.winwin.domain.entity.category.MidCategory;
 import com.dpm.winwin.domain.entity.category.SubCategory;
+import com.dpm.winwin.domain.entity.link.Link;
 import com.dpm.winwin.domain.entity.member.Member;
+import com.dpm.winwin.domain.entity.post.Post;
+import com.dpm.winwin.domain.repository.category.SubCategoryRepository;
 import com.dpm.winwin.domain.repository.member.MemberRepository;
 import com.dpm.winwin.domain.repository.member.dto.request.MemberImageRequest;
 import com.dpm.winwin.domain.repository.member.dto.request.MemberNicknameRequest;
@@ -23,6 +33,7 @@ import static com.dpm.winwin.api.common.error.enums.ErrorMessage.MEMBER_NOT_FOUN
 public class MemberCommandService {
 
     private final MemberRepository memberRepository;
+    private final SubCategoryRepository subCategoryRepository;
 
     public Long updateMemberNickname(Long memberId,
                                      MemberNicknameRequest memberNicknameRequest) {
@@ -39,12 +50,37 @@ public class MemberCommandService {
         member.updateImage(memberImageRequest.image());
         return member.getId();
     }
-    
+
     public MemberCreateResponse createMember(MemberCreateRequest memberCreateRequest){
         Member memberRequest = memberCreateRequest.toEntity();
         Member member = memberRepository.save(memberRequest);
         return MemberCreateResponse.from(member);
+    }
 
+    public MemberUpdateResponse updateMember(Long memberId,
+                                         MemberUpdateRequest memberUpdateRequest) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new BusinessException(MEMBER_NOT_FOUND));
+        List<SubCategory> takenTalents = subCategoryRepository.findAllById(
+                memberUpdateRequest.takenTalents());
+        List<SubCategory> givenTalents = subCategoryRepository.findAllById(
+                memberUpdateRequest.givenTalents());
+
+        member.update(memberUpdateRequest.toDto(),takenTalents,givenTalents);
+
+        return new MemberUpdateResponse(
+                member.getId(),
+                member.getNickname(),
+                member.getImage(),
+                member.getIntroductions(),
+                member.getProfileLink(),
+                member.getGivenTalents().stream()
+                        .map(memberTalent -> memberTalent.getTalent().getName())
+                        .toList(),
+                member.getTakenTalents().stream()
+                        .map(memberTalent -> memberTalent.getTalent().getName())
+                        .toList()
+        );
     }
 
 }

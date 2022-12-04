8c5072da224901663b3c655370332d8b4a376502
hpwh4457@naver.com <ned.choi@kakaocorp.com>
2022-12-05 03:10:09 +0900

    refactor : connection, read timeout 설정
    코드리뷰 반영
    
    #12

--- winwin-be-api/src/main/java/com/dpm/winwin/api/configuration/RestTemplateConfiguration.java
+++ winwin-be-api/src/main/java/com/dpm/winwin/api/configuration/RestTemplateConfiguration.java
@@ -2,12 +2,23 @@ package com.dpm.winwin.api.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfiguration {
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        return new RestTemplate(getClientHttpRequestFactory());
+    }
+
+    private ClientHttpRequestFactory getClientHttpRequestFactory() {
+        int timeout = 5000;
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory
+                = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setConnectTimeout(timeout);
+        clientHttpRequestFactory.setReadTimeout(timeout);
+        return clientHttpRequestFactory;
     }
 }

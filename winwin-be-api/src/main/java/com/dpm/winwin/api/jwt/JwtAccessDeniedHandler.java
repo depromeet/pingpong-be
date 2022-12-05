9c29506f62146e60a87cca50b4cc150015bd8893
hpwh4457@naver.com <ned.choi@kakaocorp.com>
2022-12-05 23:46:25 +0900

    chore : 로그 추가
    코드리뷰 반영
    
    #12

--- winwin-be-api/src/main/java/com/dpm/winwin/api/jwt/JwtAccessDeniedHandler.java
+++ winwin-be-api/src/main/java/com/dpm/winwin/api/jwt/JwtAccessDeniedHandler.java
@@ -1,5 +1,6 @@
 package com.dpm.winwin.api.jwt;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -12,12 +13,14 @@ import java.io.IOException;
 /**
  * 필요한 권한이 존재하지 않는 경우에 403 Forbidden 에러를 리턴
  */
+@Slf4j
 @Component
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
                        AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.info("허가 받지 않은 사용자의 접근입니다.");
         response.sendError(HttpServletResponse.SC_FORBIDDEN);
     }
 }

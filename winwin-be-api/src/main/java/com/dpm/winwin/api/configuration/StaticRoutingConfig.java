f858879ee05ad1eecb37b62a34ac6be5e677da30
yeonhee <ydol2@naver.com>
2022-12-08 21:17:15 +0900

    feat : swagger+restDoce 초기 설정

---
+++ winwin-be-api/src/main/java/com/dpm/winwin/api/configuration/StaticRoutingConfig.java
@@ -0,0 +1,15 @@
+package com.dpm.winwin.api.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticRoutingConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/static/swagger-ui/");
+    }
+}

7e2c6b0cf06de046f2951d597184fb3ee14e4ed2
minsoozz <minsoo1604@naver.com>
2022-12-10 17:57:54 +0900

    테스트 코드 작성 (#63)
    * refactor: 메서드명 변경
    
    * test: 테스트 코드 작성
    
    * refactor: 빌드 오류 해결
    
    - 소스 코드가 최신이 아니였습니다..
    
    * test: 카테고리 테스트 코드 작성
    
    - 테스트 코드를 열심히 작성하였습니다

--- winwin-be-api/src/main/java/com/dpm/winwin/api/post/controller/PostController.java
+++ winwin-be-api/src/main/java/com/dpm/winwin/api/post/controller/PostController.java
@@ -4,7 +4,6 @@ import com.dpm.winwin.api.common.response.dto.BaseResponseDto;
 import com.dpm.winwin.api.common.response.dto.GlobalPageResponseDto;
 import com.dpm.winwin.api.post.dto.request.PostAddRequest;
 import com.dpm.winwin.api.post.dto.request.PostUpdateRequest;
-import com.dpm.winwin.api.post.dto.response.MyPagePostListResponse;
 import com.dpm.winwin.api.post.dto.response.MyPagePostResponse;
 import com.dpm.winwin.api.post.dto.response.PostAddResponse;
 import com.dpm.winwin.api.post.dto.response.PostCustomizedResponse;
@@ -19,9 +18,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,11 +36,12 @@ public class PostController {
     public BaseResponseDto<GlobalPageResponseDto<PostListResponse>> getPostList(
         PostListConditionRequest condition, Pageable pageable
     ) {
-        GlobalPageResponseDto<PostListResponse> response = postService.getPosts(condition, pageable);
+        GlobalPageResponseDto<PostListResponse> response = postService.getPosts(condition,
+            pageable);
         return BaseResponseDto.ok(response);
     }
 
-    @PatchMapping("/{id}")
+    @PutMapping("/{id}")
     public BaseResponseDto<PostUpdateResponse> updatePost(
         @PathVariable Long id, @RequestBody final PostUpdateRequest updateRequest) {
         long memberId = 1L;
@@ -63,12 +63,12 @@ public class PostController {
         return BaseResponseDto.ok(postService.get(id, memberId));
     }
 
-  @PostMapping
-  public BaseResponseDto<PostAddResponse> createPost(@RequestBody PostAddRequest request) {
-    long tempMemberId = 1L;
-    PostAddResponse response = postService.save(tempMemberId, request);
-    return BaseResponseDto.ok(response);
-  }
+    @PostMapping
+    public BaseResponseDto<PostAddResponse> createPost(@RequestBody PostAddRequest request) {
+        long tempMemberId = 1L;
+        PostAddResponse response = postService.save(tempMemberId, request);
+        return BaseResponseDto.ok(response);
+    }
 
     @DeleteMapping("/{id}")
     public BaseResponseDto<Long> deletePost(@PathVariable Long id) {
@@ -83,7 +83,8 @@ public class PostController {
     }
 
     @GetMapping("/members/{memberId}")
-    public BaseResponseDto<GlobalPageResponseDto<MyPagePostResponse>> getAllByMemberId(@PathVariable Long memberId,
+    public BaseResponseDto<GlobalPageResponseDto<MyPagePostResponse>> getAllByMemberId(
+        @PathVariable Long memberId,
         Pageable pageable) {
         return BaseResponseDto.ok(postService.getAllByMemberId(memberId, pageable));
     }
--- winwin-be-api/src/test/java/com/dpm/winwin/api/post/controller/PostControllerTest.java
+++ winwin-be-api/src/test/java/com/dpm/winwin/api/post/controller/PostControllerTest.java
@@ -4,21 +4,27 @@ import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.dpm.winwin.api.post.dto.request.LinkRequest;
 import com.dpm.winwin.api.post.dto.request.PostAddRequest;
+import com.dpm.winwin.api.post.dto.request.PostUpdateRequest;
 import com.dpm.winwin.api.post.dto.response.LinkResponse;
 import com.dpm.winwin.api.post.dto.response.PostAddResponse;
+import com.dpm.winwin.api.post.dto.response.PostUpdateResponse;
 import com.dpm.winwin.api.post.service.PostService;
 import com.dpm.winwin.api.utils.RestDocsTestSupport;
 import com.dpm.winwin.domain.entity.post.enums.ExchangePeriod;
 import com.dpm.winwin.domain.entity.post.enums.ExchangeTime;
 import com.dpm.winwin.domain.entity.post.enums.ExchangeType;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -100,6 +106,133 @@ class PostControllerTest extends RestDocsTestSupport {
                     fieldWithPath("links").type(JsonFieldType.ARRAY).description("링크"),
                     fieldWithPath("chatLink").type(JsonFieldType.STRING).description("채팅 링크"),
                     fieldWithPath("takenTalentIds").type(JsonFieldType.ARRAY)
+                        .description("받고 싶은 재능 id"),
+                    fieldWithPath("takenContent").type(JsonFieldType.STRING)
+                        .description("받고 싶은 내용"),
+                    fieldWithPath("exchangeType").type(JsonFieldType.STRING).description("교환 방식"),
+                    fieldWithPath("exchangePeriod").type(JsonFieldType.STRING).description("교환 기간"),
+                    fieldWithPath("exchangeTime").type(JsonFieldType.STRING).description("교환 시간")
+                ),
+                responseFields(
+                    fieldWithPath("message").type(JsonFieldType.STRING).description("성공 여부"),
+                    fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("저장된 게시물 id"),
+                    fieldWithPath("data.title").type(JsonFieldType.STRING).description("제목"),
+                    fieldWithPath("data.content").type(JsonFieldType.STRING).description("내용"),
+                    fieldWithPath("data.isShare").type(JsonFieldType.BOOLEAN).description("나눔 여부"),
+                    fieldWithPath("data.mainCategory").type(JsonFieldType.STRING)
+                        .description("대분류 카테고리 이름"),
+                    fieldWithPath("data.midCategory").type(JsonFieldType.STRING)
+                        .description("중분류 카테고리 이름"),
+                    fieldWithPath("data.subCategory").type(JsonFieldType.STRING)
+                        .description("소분류 카테고리 이름"),
+                    fieldWithPath("data.links").type(JsonFieldType.ARRAY).description("링크")
+                        .optional(),
+                    fieldWithPath("data.chatLink").type(JsonFieldType.STRING).description("채팅 링크"),
+                    fieldWithPath("data.links[].id").type(JsonFieldType.NUMBER)
+                        .description("링크 id"),
+                    fieldWithPath("data.links[].content").type(JsonFieldType.STRING)
+                        .description("링크 내용"),
+                    fieldWithPath("data.takenTalents").type(JsonFieldType.ARRAY)
+                        .description("받고 싶은 카테고리"),
+                    fieldWithPath("data.takenContent").type(JsonFieldType.STRING)
+                        .description("받고 싶은 내용"),
+                    fieldWithPath("data.exchangeType").type(JsonFieldType.STRING)
+                        .description("교환 방식"),
+                    fieldWithPath("data.exchangePeriod").type(JsonFieldType.STRING)
+                        .description("교환 기간"),
+                    fieldWithPath("data.exchangeTime").type(JsonFieldType.STRING)
+                        .description("교환 시간")
+                )
+            ));
+    }
+
+    @Test
+    void post를_수정한다() throws Exception {
+
+        // given
+        Long memberId = 1L;
+        List<LinkRequest> links = new ArrayList<>();
+        links.add(new LinkRequest(1L, "www.naver.com"));
+        links.add(new LinkRequest(2L, "www.google.com"));
+
+        List<Long> takenTalents = new ArrayList<>();
+        takenTalents.add(1L);
+        takenTalents.add(2L);
+        takenTalents.add(3L);
+
+        PostUpdateRequest request = new PostUpdateRequest(
+            "제목",
+            "내용",
+            false,
+            1L,
+            links,
+            takenTalents,
+            "www.chatLink.com",
+            "받고 싶은 재능 내용",
+            ExchangeType.ONLINE,
+            ExchangePeriod.A_WEEK,
+            ExchangeTime.NOON
+        );
+
+        List<LinkResponse> linksResponse = new ArrayList<>();
+        linksResponse.add(new LinkResponse(1L, "www.naver.com"));
+        linksResponse.add(new LinkResponse(2L, "www.google.com"));
+
+        List<String> takeCategoriesResponse = linksResponse.stream()
+            .map(LinkResponse::content)
+            .collect(Collectors.toList());
+
+        PostUpdateResponse response = new PostUpdateResponse(
+            1L,
+            "제목",
+            "내용",
+            false,
+            "대분류 카테고리 이름",
+            "중분류 카테고리 이름",
+            "소분류 카테고리 이름",
+            linksResponse,
+            "www.chatLink.com",
+            takeCategoriesResponse,
+            "받고 싶은 재능 내용",
+            ExchangeType.ONLINE,
+            ExchangePeriod.A_WEEK,
+            ExchangeTime.NOON
+        );
+
+        // when
+        given(postService.update(memberId, 1L, request))
+            .willReturn(response);
+
+        ResultActions result = mockMvc.perform(
+            put("/api/v1/posts/1")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header(
+                    "memberId",
+                    1
+                )
+                .content(createJson(request))
+        );
+
+        // then
+        result.andExpect(status().is2xxSuccessful())
+            .andDo(restDocs.document(
+                requestHeaders(
+                    headerWithName("memberId").description("회원 id")
+                ),
+                requestFields(
+                    fieldWithPath("title").type(JsonFieldType.STRING).description("제목"),
+                    fieldWithPath("content").type(JsonFieldType.STRING).description("내용"),
+                    fieldWithPath("isShare").type(JsonFieldType.BOOLEAN).description("나눔 여부"),
+                    fieldWithPath("subCategoryId").type(JsonFieldType.NUMBER)
+                        .description("소분류 카테고리 id"),
+                    fieldWithPath("links").type(JsonFieldType.ARRAY).description("링크"),
+                    fieldWithPath("links[].id").type(JsonFieldType.NUMBER)
+                        .description("링크 id"),
+                    fieldWithPath("links[].content").type(JsonFieldType.STRING)
+                        .description("링크 내용"),
+                    fieldWithPath("chatLink").type(JsonFieldType.STRING).description("채팅 링크"),
+                    fieldWithPath("takenTalents").type(JsonFieldType.ARRAY)
                         .description("받고 싶은 재능 id"),
                     fieldWithPath("takenContent").type(JsonFieldType.STRING)
                         .description("받고 싶은 내용"),

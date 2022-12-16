fbca18b78b69e24f1b231f5af66de512d81d9039
yeonhee <ydol2@naver.com>
2022-12-17 01:29:49 +0900

    chore : error 메시지 알맞게 변경

--- winwin-be-api/src/main/java/com/dpm/winwin/api/common/error/enums/ErrorMessage.java
+++ winwin-be-api/src/main/java/com/dpm/winwin/api/common/error/enums/ErrorMessage.java
@@ -24,7 +24,7 @@ public enum ErrorMessage {
     POST_NOT_FOUND(NOT_FOUND, "해당 포스트를 찾을 수 없습니다."),
     TALENT_NOT_FOUND(NOT_FOUND, "해당 받고 싶은 재능을 찾을 수 없습니다."),
     LINK_NOT_FOUND(NOT_FOUND, "해당 링크를 찾을 수 없습니다."),
-    LIKE_NOT_FOUND(NOT_FOUND, "해당 링크를 찾을 수 없습니다."),
+    LIKE_NOT_FOUND(NOT_FOUND, "해당 좋아요를 찾을 수 없습니다."),
     DOES_NOT_MATCH_NONCE(BAD_REQUEST, "ID_TOKEN 값 중 NONCE 값이 일치하지 않습니다."),
     INVALID_ISSUER_VALUE(BAD_REQUEST, "ISSUER 값이 잘못되었습니다."),
     INVALID_CLIENT_ID(BAD_REQUEST, "클라이언트 아이디가 잘못되었습니다."),

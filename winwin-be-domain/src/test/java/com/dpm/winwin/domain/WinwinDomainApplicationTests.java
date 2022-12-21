6e6e7eb0ae6ecc5859c047d6cb372be18d69042f
서명현 <azure394@gmail.com>
2022-12-22 01:15:35 +0900

    feat: domain 모듈 테스트 환경 세팅 (#95)
    * build: test 관련 라이브러리 root 모듈로 위치 변경
    
    * chore: application.yml 설정
    
    * test: 테스트 환경 세팅
    
    * refactor: 코드 리뷰 반영

---
+++ winwin-be-domain/src/test/java/com/dpm/winwin/domain/WinwinDomainApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dpm.winwin.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WinwinDomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}

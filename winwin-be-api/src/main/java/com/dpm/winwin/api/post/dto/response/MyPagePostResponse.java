83d0adae5cce589eb278abf0c3416a9eaaa9faf0
sa46lll <9812smh@naver.com>
2022-12-26 16:03:40 +0900

    feat: 좋아요 수 string 포맷 변경

--- winwin-be-api/src/main/java/com/dpm/winwin/api/post/dto/response/MyPagePostResponse.java
+++ winwin-be-api/src/main/java/com/dpm/winwin/api/post/dto/response/MyPagePostResponse.java
@@ -1,18 +1,27 @@
 package com.dpm.winwin.api.post.dto.response;
 
+import static com.dpm.winwin.domain.entity.post.Likes.changeFormatCountToString;
+
 import com.dpm.winwin.domain.dto.post.MyPagePostDto;
 import java.util.List;
 
-public record MyPagePostResponse(Long id,
-                                 String title,
-                                 String subCategory,
-                                 boolean isShare,
-                                 List<String> takenTalents,
-                                 int likes) {
+public record MyPagePostResponse(
+    Long id,
+    String title,
+    String subCategory,
+    boolean isShare,
+    List<String> takenTalents,
+    String likes
+) {
 
     public static MyPagePostResponse of(MyPagePostDto myPagePostDto) {
-        return new MyPagePostResponse(myPagePostDto.id(), myPagePostDto.title(),
-            myPagePostDto.subCategory(), myPagePostDto.isShare(), myPagePostDto.takenTalents(),
-            myPagePostDto.likes());
+        return new MyPagePostResponse(
+            myPagePostDto.id(),
+            myPagePostDto.title(),
+            myPagePostDto.subCategory(),
+            myPagePostDto.isShare(),
+            myPagePostDto.takenTalents(),
+            changeFormatCountToString(myPagePostDto.likes())
+        );
     }
 }

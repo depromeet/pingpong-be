0d4cda4bd8a1ef6376e77c709dc0eb4b944b2631
서명현 <azure394@gmail.com>
2022-12-28 01:14:28 +0900

    feat: 멤버 프로필 수정 응답 수정 (#113)
    * feat: talent id 응답값 포함
    
    * test: 테스트 코드 수정

--- winwin-be-api/src/test/java/com/dpm/winwin/api/member/controller/MemberControllerTest.java
+++ winwin-be-api/src/test/java/com/dpm/winwin/api/member/controller/MemberControllerTest.java
@@ -8,6 +8,7 @@ import com.dpm.winwin.api.member.dto.request.MemberUpdateRequest;
 import com.dpm.winwin.api.member.dto.response.MemberNicknameResponse;
 import com.dpm.winwin.api.member.dto.response.MemberRankReadResponse;
 import com.dpm.winwin.api.member.dto.response.MemberUpdateResponse;
+import com.dpm.winwin.api.member.dto.response.TalentResponse;
 import com.dpm.winwin.api.member.service.MemberCommandService;
 import com.dpm.winwin.api.member.service.MemberQueryService;
 import com.dpm.winwin.api.utils.RestDocsTestSupport;
@@ -145,8 +146,8 @@ public class MemberControllerTest extends RestDocsTestSupport {
                 "https://dpm-pingpong-bucket.s3.ap-northeast-2.amazonaws.com/profileImage/3d4395e461db40108104200e286870c4-kirby.png",
                 "안녕하세요 김감자입니다.",
                 "www.depromeet.com",
-                List.of("자소서 · 면접", "취업 · 이직 · 진로", "기획 · PM"),
-                List.of("영업 · MD", "보고서 · 발표", "생산성 툴")
+                List.of(new TalentResponse(1L, "자소서·면접"), new TalentResponse(2L, "취업·이직·진로")),
+                List.of(new TalentResponse(1L, "자소서·면접"), new TalentResponse(2L, "취업·이직·진로"))
         );
 
         // when
@@ -186,10 +187,14 @@ public class MemberControllerTest extends RestDocsTestSupport {
                                         .description("회원 소개"),
                                 fieldWithPath("data.profileLink").type(JsonFieldType.STRING)
                                         .description("회원의 대표 링크"),
-                                fieldWithPath("data.givenTalents").type(JsonFieldType.ARRAY)
-                                        .description("회원이 가진 재능"),
-                                fieldWithPath("data.takenTalents").type(JsonFieldType.ARRAY)
-                                        .description("회원이 줄 수 있는 재능")
+                                fieldWithPath("data.givenTalents[].id").type(JsonFieldType.NUMBER)
+                                        .description("회원이 가진 재능 id"),
+                                fieldWithPath("data.givenTalents[].content").type(JsonFieldType.STRING)
+                                    .description("회원이 가진 재능 내용"),
+                                fieldWithPath("data.takenTalents[].id").type(JsonFieldType.NUMBER)
+                                        .description("회원이 줄 수 있는 재능 id"),
+                                fieldWithPath("data.takenTalents[].content").type(JsonFieldType.STRING)
+                                    .description("회원이 줄 수 있는 재능 내용")
                         )
                 ));
     }

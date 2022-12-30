0f791ba0d42d7344646e4d31da869115f45bc73b
sa46lll <9812smh@naver.com>
2022-12-30 23:59:39 +0900

    feat: rank 이미지 url 변경

--- winwin-be-domain/src/main/java/com/dpm/winwin/domain/entity/member/enums/Ranks.java
+++ winwin-be-domain/src/main/java/com/dpm/winwin/domain/entity/member/enums/Ranks.java
@@ -1,14 +1,15 @@
 package com.dpm.winwin.domain.entity.member.enums;
 
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 
 @Getter
 public enum Ranks {
 
-    PRO("프로", "rank/pro.png", "좋아요 10k 이상", 10000),
-    JUNIOR("주니어", "rank/junior.png", "좋아요 1k 이상", 1000),
-    BEGINNER("비기너", "rank/beginner.png", "좋아요 100개 이상", 100),
-    ROOKIE("루키", "rank/rookie.png", "좋아요 100개 미만", 0);
+    PRO("프로", "https://dpm-pingpong-bucket.s3.ap-northeast-2.amazonaws.com/rank/pro.png", "좋아요 10k 이상", 10000),
+    JUNIOR("주니어", "https://dpm-pingpong-bucket.s3.ap-northeast-2.amazonaws.com/rank/junior.png", "좋아요 1k 이상", 1000),
+    BEGINNER("비기너", "https://dpm-pingpong-bucket.s3.ap-northeast-2.amazonaws.com/rank/beginner.png", "좋아요 100개 이상", 100),
+    ROOKIE("루키", "https://dpm-pingpong-bucket.s3.ap-northeast-2.amazonaws.com/rank/rookie.png", "좋아요 100개 미만", 0);
 
     private final String name;
     private final String image;

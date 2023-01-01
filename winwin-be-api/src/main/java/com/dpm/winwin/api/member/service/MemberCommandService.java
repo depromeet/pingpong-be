ea0e5d5248ea13ce447ff7dd2c90794fb382c476
0Kwon Choi <47075043+choiyoungkwon12@users.noreply.github.com>
2023-01-01 17:36:34 +0900

    refactor : 회원 탈퇴 시 사유 받도록 변경 (#121)
    * refactor : 회원 탈퇴 시 사유 받도록 변경
    
    * refactor : 회원 탈퇴 시 사유 변수명 변경
    
    * refactor : 회원 탈퇴 시 사유 받도록 변경
    
    * refactor : 회원 탈퇴 시 사유 변수명 변경
    
    * refactor : ClientRegistration 검색 못하는 부분 임시 처리
    
    Co-authored-by: hpwh4457@naver.com <ned.choi@kakaocorp.com>

--- winwin-be-api/src/main/java/com/dpm/winwin/api/member/service/MemberCommandService.java
+++ winwin-be-api/src/main/java/com/dpm/winwin/api/member/service/MemberCommandService.java
@@ -21,6 +21,7 @@ import com.dpm.winwin.domain.repository.category.SubCategoryRepository;
 import com.dpm.winwin.domain.repository.member.MemberRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -92,15 +93,12 @@ public class MemberCommandService {
         );
     }
 
-    public MemberDeleteResponse deleteMember(Long memberId) {
+    public MemberDeleteResponse deleteMember(Long memberId, String content) {
 
         Member member = memberRepository.findMemberWithToken(memberId)
             .orElseThrow(() -> new BusinessException(MEMBER_NOT_FOUND));
 
-        OauthToken oauthToken = member.getOauthToken();
-        String refreshToken = oauthToken.getRefreshToken();
-
-        ResponseEntity<Object> response = appleTokenRevokeRequest(oauthToken.getProviderType().name(), refreshToken);
+        ResponseEntity<Object> response = revokeAppleToken(member);
 
         if (response.getStatusCode().equals(HttpStatus.OK)) {
             memberRepository.delete(member);
@@ -112,8 +110,16 @@ public class MemberCommandService {
         throw new BusinessException(APPLE_TOKEN_REVOKE_FAIL);
     }
 
+    @NotNull
+    private ResponseEntity<Object> revokeAppleToken(Member member) {
+        OauthToken oauthToken = member.getOauthToken();
+        String refreshToken = oauthToken.getRefreshToken();
+
+        return appleTokenRevokeRequest(oauthToken.getProviderType().name(), refreshToken);
+    }
+
     private ResponseEntity<Object> appleTokenRevokeRequest(String providerName, String refreshToken) {
-        ClientRegistration registration = clientRegistrationRepository.findByRegistrationId(providerName);
+        ClientRegistration registration = clientRegistrationRepository.findByRegistrationId(providerName.toLowerCase());
         String clientSecret = registration.getClientSecret();
         String clientId = registration.getClientId();
 

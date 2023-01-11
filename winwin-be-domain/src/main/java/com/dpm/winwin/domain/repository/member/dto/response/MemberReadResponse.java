0d80a39e0e8939c0c0de6fe7aff9a0b626c84b86
ned-choi <ned.choi@kakaocorp.com>
2023-01-11 21:03:17 +0900

    refactor : 사용자 조회 시 애플에서 제공해준 이름도 응답 데이터에 추가

--- winwin-be-domain/src/main/java/com/dpm/winwin/domain/repository/member/dto/response/MemberReadResponse.java
+++ winwin-be-domain/src/main/java/com/dpm/winwin/domain/repository/member/dto/response/MemberReadResponse.java
@@ -3,6 +3,7 @@ package com.dpm.winwin.domain.repository.member.dto.response;
 import com.dpm.winwin.domain.entity.member.enums.Ranks;
 
 public record MemberReadResponse(Long memberId,
+                                 String name,
                                  String nickname,
                                  String image,
                                  String introduction,
